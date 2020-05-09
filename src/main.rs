da1711ad89a274a9fcf6fda5044645f7d11fb93a
Eloy <zer1t0ps@protonmail.com>
2020-05-09 11:16:09 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,271 @@
+use chrono::{Duration, Utc};
+use clap::{App, Arg, ArgGroup, ArgMatches};
+use kerbeiros;
+use kerberos_asn1;
+use kerberos_asn1::Asn1Object;
+use kerberos_constants;
+use rand;
+use rand::Rng;
+use std::io;
+use std::io::{Read, Write};
+use std::net::IpAddr;
+use std::net::{SocketAddr, TcpStream};
+use std::time;
+
+fn args() -> App<'static, 'static> {
+    App::new(env!("CARGO_PKG_NAME"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::with_name("domain")
+                .long("domain")
+                .short("d")
+                .takes_value(true)
+                .help("Domain for request the ticket")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("user")
+                .long("user")
+                .short("u")
+                .takes_value(true)
+                .help("Username for request the ticket")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("password")
+                .long("password")
+                .short("p")
+                .takes_value(true)
+                .help("Password of user"),
+        )
+        .arg(
+            Arg::with_name("ntlm")
+                .long("ntlm")
+                .takes_value(true)
+                .help("NTLM hash of user"),
+        )
+        .arg(
+            Arg::with_name("aes-128")
+                .long("aes-128")
+                .takes_value(true)
+                .help("AES 128 Kerberos key of user"),
+        )
+        .arg(
+            Arg::with_name("aes-256")
+                .long("aes-256")
+                .takes_value(true)
+                .help("AES 256 Kerberos key of user"),
+        )
+        .group(
+            ArgGroup::with_name("user_key")
+                .args(&["password", "ntlm", "aes-128", "aes-256"])
+                .multiple(false),
+        )
+        .arg(
+            Arg::with_name("kdc-ip")
+                .long("kdc-ip")
+                .short("k")
+                .value_name("ip")
+                .takes_value(true)
+                .required(true)
+                .help("The address of the KDC"),
+        )
+        .arg(
+            Arg::with_name("ticket-format")
+                .long("ticket-format")
+                .takes_value(true)
+                .possible_values(&["krb", "ccache"])
+                .help("Format to save retrieved tickets.")
+                .default_value("ccache"),
+        )
+        .arg(
+            Arg::with_name("out-file")
+                .long("out-file")
+                .takes_value(true)
+                .value_name("file")
+                .help("File to save TGT."),
+        )
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum TicketFormat {
+    Krb,
+    Ccache,
+}
+
+pub struct Arguments {
+    pub domain: String,
+    pub username: String,
+    pub user_password: Option<String>,
+    pub user_key: Option<kerbeiros::Key>,
+    pub kdc_ip: IpAddr,
+}
+
+pub struct ArgumentsParser<'a> {
+    matches: &'a ArgMatches<'a>,
+}
+
+impl<'a> ArgumentsParser<'a> {
+    pub fn parse(matches: &'a ArgMatches) -> Arguments {
+        let parser = Self { matches: matches };
+        return parser._parse();
+    }
+
+    fn _parse(&self) -> Arguments {
+        let domain = self.matches.value_of("domain").unwrap().into();
+        let username = self.matches.value_of("user").unwrap().into();
+        let user_key = self.parse_user_key();
+        let kdc_ip = self.parse_kdc_ip();
+        let user_password = self.parse_user_password();
+
+        return Arguments {
+            domain,
+            username,
+            user_key,
+            kdc_ip,
+            user_password
+        };
+    }
+
+    fn parse_kdc_ip(&self) -> IpAddr {
+        let kdc_ip = self.matches.value_of("kdc-ip").unwrap();
+        return kdc_ip.parse::<IpAddr>().unwrap();
+    }
+
+    fn parse_user_password(&self) -> Option<String> {
+        return Some(self.matches.value_of("password")?.into());
+    }
+
+    fn parse_user_key(&self) -> Option<kerbeiros::Key> {
+        if let Some(password) = self.matches.value_of("password") {
+            return Some(kerbeiros::Key::Password(password.to_string()));
+        } else if let Some(ntlm) = self.matches.value_of("ntml") {
+            return Some(kerbeiros::Key::from_rc4_key_string(ntlm).unwrap());
+        } else if let Some(aes_128_key) = self.matches.value_of("aes-128") {
+            return Some(
+                kerbeiros::Key::from_aes_128_key_string(aes_128_key).unwrap(),
+            );
+        } else if let Some(aes_256_key) = self.matches.value_of("aes-256") {
+            return Some(
+                kerbeiros::Key::from_aes_256_key_string(aes_256_key).unwrap(),
+            );
+        }
+        return None;
+    }
+}
+
+fn main() {
+    let args = ArgumentsParser::parse(&args().get_matches());
+
+    let mut as_req = kerberos_asn1::AsReq::default();
+
+    let mut padatas = Vec::new();
+
+    // request PAC in response
+    padatas.push(kerberos_asn1::PaData::new(
+        kerberos_constants::pa_data_types::PA_PAC_REQUEST,
+        kerberos_asn1::KerbPaPacRequest::new(true).build(),
+    ));
+
+    // request padata
+    as_req.padata = Some(padatas);
+
+    as_req.req_body.etypes = vec![
+        kerberos_constants::etypes::RC4_HMAC,
+        kerberos_constants::etypes::AES128_CTS_HMAC_SHA1_96,
+        kerberos_constants::etypes::AES256_CTS_HMAC_SHA1_96,
+    ];
+
+    as_req.req_body.kdc_options = kerberos_asn1::KerberosFlags::from(
+        kerberos_constants::kdc_options::FORWARDABLE
+            | kerberos_constants::kdc_options::RENEWABLE
+            | kerberos_constants::kdc_options::CANONICALIZE
+            | kerberos_constants::kdc_options::RENEWABLE_OK,
+    );
+
+    // set username
+    as_req.req_body.cname = Some(kerberos_asn1::PrincipalName {
+        name_type: kerberos_constants::principal_names::NT_PRINCIPAL,
+        name_string: vec![args.username],
+    });
+
+    // set the target realm
+    as_req.req_body.realm = args.domain.clone();
+
+    // set the target service
+    as_req.req_body.sname = Some(kerberos_asn1::PrincipalName {
+        name_type: kerberos_constants::principal_names::NT_PRINCIPAL,
+        name_string: vec!["krbtgt".into(), args.domain],
+    });
+
+    as_req.req_body.rtime = Some(
+        Utc::now()
+            .checked_add_signed(Duration::weeks(20 * 52))
+            .unwrap()
+            .into(),
+    );
+
+    as_req.req_body.till = Utc::now()
+        .checked_add_signed(Duration::weeks(20 * 52))
+        .unwrap()
+        .into();
+
+    as_req.req_body.nonce = rand::thread_rng().gen::<u32>();
+
+    let socket_addr = SocketAddr::new(args.kdc_ip, 88);
+    let raw_as_req = as_req.build();
+
+    let raw_response = send_recv_tcp(&socket_addr, &raw_as_req)
+        .expect("Error in send_recv_tcp");
+
+    let (_, krb_error) = kerberos_asn1::KrbError::parse(&raw_response)
+        .expect("Error parsing raw_response");
+
+    let error_string =  kerberos_constants::error_codes::error_code_to_string(krb_error.error_code);
+        
+    eprintln!(" Error {}: {}", krb_error.error_code, error_string);
+}
+
+fn send_recv_tcp(
+    dst_addr: &SocketAddr,
+    raw_request: &[u8],
+) -> io::Result<Vec<u8>> {
+    let mut tcp_stream =
+        TcpStream::connect_timeout(dst_addr, time::Duration::new(5, 0))?;
+
+    let raw_sized_request = set_size_header_to_request(raw_request);
+    tcp_stream.write(&raw_sized_request)?;
+
+    let mut len_data_bytes = [0 as u8; 4];
+    tcp_stream.read_exact(&mut len_data_bytes)?;
+    let data_length = u32::from_be_bytes(len_data_bytes);
+
+    let mut raw_response: Vec<u8> = vec![0; data_length as usize];
+    tcp_stream.read_exact(&mut raw_response)?;
+
+    return Ok(raw_response);
+}
+
+fn set_size_header_to_request(raw_request: &[u8]) -> Vec<u8> {
+    let request_length = raw_request.len() as u32;
+    let mut raw_sized_request: Vec<u8> = request_length.to_be_bytes().to_vec();
+    raw_sized_request.append(&mut raw_request.to_vec());
+
+    return raw_sized_request;
+}
+
+// añadir esta y funcion que devuelva los supported etypes en kerberos cryptox
+fn generate_aes_salt(realm: &str, client_name: &str) -> Vec<u8> {
+    let mut salt = realm.to_uppercase();
+    let mut lowercase_username = client_name.to_lowercase();
+
+    if lowercase_username.ends_with("$") {
+        salt.push_str("host");
+        lowercase_username.pop();
+    }
+    salt.push_str(&lowercase_username);
+
+    return salt.as_bytes().to_vec();
+}
